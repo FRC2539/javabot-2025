4ae66eaf9a48f367de9da94644d10faecd85c90c
Voxel <jamesgormleyrechs@gmail.com>
2024-12-11 20:08:38 -0500

    Fix AdvantageKit Logging, LoggedSendableChooser
    Fixed AdvantageKit logging by removing invalid log replay code.
    
    Replaced standard sendableChooser with loggedSendableChooser

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -34,9 +34,6 @@ public class Robot extends LoggedRobot {
       PowerDistribution distribution = new PowerDistribution(1, ModuleType.kRev); // Enables power distribution logging
     } else {
       setUseTiming(false); // Run as fast as possible
-      String logPath = LogFileUtil.findReplayLog(); // Pull the replay log from AdvantageScope (or prompt the user)
-      Logger.setReplaySource(new WPILOGReader(logPath)); // Read replay log
-      Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim"))); // Save outputs to a new log
     }
 
     Logger.start(); // Start logging! No more data receivers, replay sources, or metadata values may be added.
