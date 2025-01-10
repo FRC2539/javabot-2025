9011fc4a5ed8fde490817296dc318e6f8c4a0572
Matthew Milunic <62996888+mmilunicmobile@users.noreply.github.com>
2025-01-10 12:12:37 -0500

    Update generate_svg.sh

--- stats_tracker/generate_svg.sh
+++ stats_tracker/generate_svg.sh
@@ -9,12 +9,12 @@ echo "Deletion(s): $d"
 value=$(($i - $d))
 echo "Lines of code: $value"
 mkdir build
-echo "<svg width=\"300px\" height=\"300px\" xmlns=\"http://www.w3.org/2000/svg\">
+echo "<svg width=\"300px\" height=\"200px\" xmlns=\"http://www.w3.org/2000/svg\">
     <style>
         @import url(\"https://fonts.googleapis.com/css?family=Source+Code+Pro\");
     </style>
-    <image href=\"background.png\" width=\"100%\" height=\"100%\"/>
-    <text x=\"50%\" y=\"90px\" font-size=\"65\" text-anchor=\"middle\" fill=\"black\" font-family=\"Source Code Pro\" font-weight=\"bold\">$value</text>
+    <image href=\"https://picsum.photos/300/200\" width=\"100%\" height=\"100%\"/>
+    <text x=\"50%\" y=\"90px\" font-size=\"65\" text-anchor=\"middle\" fill=\"black\" font-family=\"Source Code Pro:700\" font-weight=\"bold\">$value</text>
 </svg>" > loc.svg
 # TEMP_DEB="$(mktemp)" &&
 # wget -O "$TEMP_DEB" 'https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/wkhtmltox_0.12.6.1-2.jammy_amd64.deb' &&
