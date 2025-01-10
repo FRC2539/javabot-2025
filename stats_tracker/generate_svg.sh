32975c72e378b655671901636520663309ea1d5e
Matthew Milunic <62996888+mmilunicmobile@users.noreply.github.com>
2025-01-10 11:28:33 -0500

    Update generate_svg.sh

--- stats_tracker/generate_svg.sh
+++ stats_tracker/generate_svg.sh
@@ -13,7 +13,7 @@ echo "<svg width=\"300px\" height=\"300px\" xmlns=\"http://www.w3.org/2000/svg\"
     <style>
         @import url(\"https://fonts.googleapis.com/css?family=Source+Code+Pro\");
     </style>
-    <image href=\"background.png\" width=\"100%\"/>
+    <image href=\"background.png\" width=\"100%\" height=\"100%\"/>
     <text x=\"50%\" y=\"90px\" font-size=\"65\" text-anchor=\"middle\" fill=\"black\" font-family=\"Source Code Pro\" font-weight=\"bold\">$value</text>
 </svg>" > loc.svg
 # TEMP_DEB="$(mktemp)" &&
