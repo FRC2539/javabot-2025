6312d86d2c1d0fbbb34f7db798cb8b7df2d5e8ed
AlexNunemacher <apullinune@gmail.com>
2025-01-18 11:16:56 -0500

    no follower

--- src/main/java/frc/robot/subsystems/elevator/ElevatorIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIOTalonFX.java
@@ -3,15 +3,14 @@ package frc.robot.subsystems.elevator;
 import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfigurator;
-import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import frc.robot.constants.ElevatorConstants;
 
 public class ElevatorIOTalonFX implements ElevatorIO {
     // TBD: Hardcode IDs or add support to make changeable in method
-    private final TalonFX elevatorLeader = new TalonFX(41, "CANivore");
-    private final TalonFX elevatorFollower = new TalonFX(42, "CANivore");
+    private final TalonFX elevatorLeader = new TalonFX(9, "CANivore");
+    // private final TalonFX elevatorFollower = new TalonFX(42, "CANivore");
     private final MotionMagicVoltage motionMagicVoltage = new MotionMagicVoltage(0);
 
     public ElevatorIOTalonFX() {
@@ -29,7 +28,7 @@ public class ElevatorIOTalonFX implements ElevatorIO {
                         .withSlot0(ElevatorConstants.slot0Configs)
                         .withMotionMagic(ElevatorConstants.motionMagicConfigs));
 
-        elevatorFollower.setControl(new Follower(elevatorFollower.getDeviceID(), false));
+        // elevatorFollower.setControl(new Follower(elevatorFollower.getDeviceID(), false));
     }
 
     public void updateInputs(ElevatorIOInputs inputs) {
