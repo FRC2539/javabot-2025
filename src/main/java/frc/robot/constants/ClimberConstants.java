38b67036eae097b620d4e44f26b8243dd4f21023
AlexNunemacher <147670490+AlexNunemacher@users.noreply.github.com>
2025-01-28 18:27:23 -0500

    129 climber (#133)
    * Climber
    
    * Gravity type
    
    * Ran spotless to fix formatting
    
    ---------
    
    Co-authored-by: li-pearl <anshipaul@outlook.com>

---
+++ src/main/java/frc/robot/constants/ClimberConstants.java
@@ -0,0 +1,22 @@
+package frc.robot.constants;
+
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+
+public class ClimberConstants {
+    public static final Slot0Configs slot0Configs =
+            new Slot0Configs()
+                    .withKA(0)
+                    .withKD(0)
+                    .withKG(0)
+                    .withKI(0)
+                    .withKP(0)
+                    .withKS(0)
+                    .withKV(0);
+    ;
+    public static final MotionMagicConfigs motionMagicConfigs =
+            new MotionMagicConfigs()
+                    .withMotionMagicAcceleration(4) // these are guesses, come back here
+                    .withMotionMagicCruiseVelocity(4) // also guess
+                    .withMotionMagicJerk(4);
+}
