257999b983b9f15a3c065550a58012ecf4db3855
mmilunicmobile <mmilunicmobile@gmail.com>
2025-01-29 21:00:33 -0500

    we are on god the rizziesrt in the house lowk - ray

--- src/main/java/frc/robot/constants/GripperConstants.java
+++ src/main/java/frc/robot/constants/GripperConstants.java
@@ -1,5 +1,11 @@
 package frc.robot.constants;
 
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+
 public class GripperConstants {
     public static final int id = 12;
+
+    public static final CurrentLimitsConfigs currentLimit = new CurrentLimitsConfigs().withStatorCurrentLimit(50);
+
+    public static final String canbus = "CANivore";
 }
--- src/main/java/frc/robot/subsystems/gripper/GripperIOFalcon.java
+++ src/main/java/frc/robot/subsystems/gripper/GripperIOFalcon.java
@@ -1,18 +1,27 @@
 package frc.robot.subsystems.gripper;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
 import frc.robot.constants.GripperConstants;
 
 public class GripperIOFalcon implements GripperIO {
-    private final TalonFX armRoller = new TalonFX(GripperConstants.id);
+    private final TalonFX armRoller = new TalonFX(GripperConstants.id, GripperConstants.canbus);
 
     public GripperIOFalcon() {
         armRoller.setPosition(0);
+
+        armRoller.getConfigurator().apply(new TalonFXConfiguration().withCurrentLimits(GripperConstants.currentLimit));
+
+        armRoller.setNeutralMode(NeutralModeValue.Brake);
     }
 
     public void updateInputs(GripperIOInputs inputs) {
-        inputs.speed = armRoller.getVelocity().refresh().getValueAsDouble();
-        inputs.voltage = armRoller.getMotorVoltage().refresh().getValueAsDouble();
+        inputs.speed = armRoller.getVelocity().getValueAsDouble();
+        inputs.voltage = armRoller.getMotorVoltage().getValueAsDouble();
+        inputs.current = armRoller.getStatorCurrent().getValueAsDouble();
+        inputs.temperature = armRoller.getDeviceTemp().getValueAsDouble();
     }
 
     public void setVoltage(double voltage) {
