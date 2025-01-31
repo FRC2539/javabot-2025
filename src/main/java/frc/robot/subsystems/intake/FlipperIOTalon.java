bae3b74f38128bc21a494a54bbbc8f23273ec339
wibwib <ray@dokholyan.org>
2025-01-30 20:11:27 -0500

    workthrough for the sigma intake . net
    Co-authored-by: Rishi Reddy <RishiReddy1002@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/intake/FlipperIOTalon.java
+++ src/main/java/frc/robot/subsystems/intake/FlipperIOTalon.java
@@ -1,17 +1,30 @@
 package frc.robot.subsystems.intake;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import frc.robot.constants.IntakeConstants;
 
 public class FlipperIOTalon implements FlipperIO {
-    private final TalonFX flipperMotor = new TalonFX(14);
-    private final PositionVoltage openPV = new PositionVoltage(100);
-    private final PositionVoltage closedPV = new PositionVoltage(0);
+    private final TalonFX flipperMotor = new TalonFX(IntakeConstants.idFlipper);
+    private final PositionVoltage openPV = new PositionVoltage(IntakeConstants.openPosit);
+    private final PositionVoltage closedPV = new PositionVoltage(IntakeConstants.closedPosit);
+
+    public FlipperIOTalon() {
+
+        flipperMotor.getConfigurator().apply(new TalonFXConfiguration().withCurrentLimits(IntakeConstants.currentLimit));
+
+        flipperMotor.setNeutralMode(NeutralModeValue.Brake);
+    }
 
     public void updateInputs(FlipperIOInputs inputs) {
-        inputs.position = flipperMotor.getPosition().refresh().getValueAsDouble();
-        inputs.voltage = flipperMotor.getMotorVoltage().refresh().getValueAsDouble();
-    }
+        inputs.position = flipperMotor.getPosition().getValueAsDouble();
+        inputs.voltage = flipperMotor.getMotorVoltage().getValueAsDouble();
+        inputs.temperature = flipperMotor.getDeviceTemp().getValueAsDouble();
+        inputs.current = flipperMotor.getStatorCurrent().getValueAsDouble();
+    } 
 
     public void setOpen() {
 
