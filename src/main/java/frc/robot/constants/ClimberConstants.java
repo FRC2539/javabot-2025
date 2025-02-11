7a7cd67baa58cec4bc82b94d451c102ad6047d27
Asa Wang <axxwang13@gmail.com>
2025-02-10 20:48:36 -0500

    climber head bones

--- src/main/java/frc/robot/constants/ClimberConstants.java
+++ src/main/java/frc/robot/constants/ClimberConstants.java
@@ -27,7 +27,11 @@ public class ClimberConstants {
 
     public static final double lowerLimit = 0;
 
-    public static final int id = 83;
+    public static final int CLIMBER_ID = 83;
+
+    public static final int CLIMBER_HEAD_ID = 0; // or 40? the electrical manual wasn't clear
+
+    public static final double ClimberHeadCurrent = 0;
 
     public static final String CANbus = "CANivore";
 }
