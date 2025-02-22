63113a8e26808c2612ef015fe6b4127505be318f
mmilunicmobile <mmilunicmobile@gmail.com>
2025-02-22 09:53:48 -0500

    YAY

--- src/main/java/frc/robot/subsystems/wrist/WristIONeo550.java
+++ src/main/java/frc/robot/subsystems/wrist/WristIONeo550.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.wrist;
 
+import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel;
@@ -18,7 +19,7 @@ public class WristIONeo550 implements WristIO {
             new DutyCycleEncoder(WristConstants.WRIST_THROUGHBORE_ENCODER_ID, 2 * Math.PI, 0);
 
     public WristIONeo550() {
-        wristMotor.getEncoder().setPosition(0);
+        wristMotor.getEncoder().setPosition(-Math.PI / 2 / Math.PI * 76.1);
 
         SparkBaseConfig config =
                 new SparkMaxConfig()
@@ -33,12 +34,15 @@ public class WristIONeo550 implements WristIO {
 
     private double lastVoltage = 0;
 
+    private boolean usingVoltage = true;
+
     public void updateInputs(WristIOInputs inputs) {
-        inputs.position = wristMotor.getEncoder().getPosition();
         inputs.voltage = wristMotor.getBusVoltage() * wristMotor.getAppliedOutput();
         inputs.current = wristMotor.getOutputCurrent();
         inputs.temperature = wristMotor.getMotorTemperature();
         inputs.throughboreEncoderPosition = throughboreEncoder.get() - 4.22;
+        wristMotor.getEncoder().setPosition(inputs.throughboreEncoderPosition  / Math.PI * 76.1);
+        inputs.position = wristMotor.getEncoder().getPosition();
         inputs.throughboreConnected = throughboreEncoder.isConnected();
 
         if (inputs.temperature > 60) {
@@ -49,23 +53,35 @@ public class WristIONeo550 implements WristIO {
 
         inputs.shutdown = shutdown;
 
-        if (inputs.throughboreEncoderPosition >= WristConstants.upperLimit && lastVoltage > 0) {
+        if (inputs.throughboreEncoderPosition >= WristConstants.upperLimit && (inputs.voltage > 0 || lastVoltage > 0)) {
             lastVoltage = 0;
+            usingVoltage = true;
         }
-        if (inputs.throughboreEncoderPosition <= WristConstants.lowerLimit && lastVoltage < 0) {
+        if (inputs.throughboreEncoderPosition <= WristConstants.lowerLimit && (inputs.voltage < 0 || lastVoltage < 0)) {
             lastVoltage = 0;
+            usingVoltage = true;
         }
         if (!inputs.throughboreConnected) {
             lastVoltage = 0;
+            usingVoltage = true;
         }
         if (shutdown) {
             lastVoltage = 0;
+            usingVoltage = true;
         }
 
-        wristMotor.setVoltage(lastVoltage);
+        if (usingVoltage) {
+            wristMotor.setVoltage(lastVoltage);
+        }
     }
 
     public void setVoltage(double voltage) {
         lastVoltage = voltage;
+        usingVoltage = true;
+    }
+
+    public void setPositionControl(double reference) {
+        wristMotor.getClosedLoopController().setReference(reference / Math.PI * 76.1, ControlType.kPosition);
+        usingVoltage = false;
     }
 }
